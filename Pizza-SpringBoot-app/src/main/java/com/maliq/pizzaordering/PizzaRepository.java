0983aebcd400410821ace4bca017fba20819d1a2
Riswan <ssmriswanmaliq@gmail.com>
2021-10-02 17:19:38 +0530

    Changes

--- Pizza-SpringBoot-app/src/main/java/com/maliq/pizzaordering/PizzaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.maliq.pizzaordering;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
-
-}
