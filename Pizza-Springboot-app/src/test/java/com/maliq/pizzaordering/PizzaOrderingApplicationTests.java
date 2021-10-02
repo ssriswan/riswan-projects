4982c4224622b6564cbade2aba0fcfc1b1cc72eb
Riswan <ssmriswanmaliq@gmail.com>
2021-10-02 17:21:31 +0530

    Backedn added

---
+++ Pizza-Springboot-app/src/test/java/com/maliq/pizzaordering/PizzaOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maliq.pizzaordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaOrderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
